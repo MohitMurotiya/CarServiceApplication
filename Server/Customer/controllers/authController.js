41101c0fc885d4dad454b16c413dd0547245c788
Mohit Murotiya <mohitmurotiya98@gmail.com>
2020-09-01 09:25:12 +0530

    Auth Updated

--- Server/Customer/controllers/authController.js
+++ Server/Customer/controllers/authController.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const authConfig = require("../config/authConfig");
 
 exports.register = (req, res, next) => {
+  console.log("Inside Register");
+
   if (!req.body.name || !req.body.email || !req.body.password) {
     return res.status(400).json({ message: "All fields are required" });
   }
