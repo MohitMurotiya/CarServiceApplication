61cafab01178b846a13bd52a8d2b025daf3195ad
Mohit Murotiya <mohitmurotiya98@gmail.com>
2020-09-01 09:28:59 +0530

    Car Services

---
+++ Client/car-wash-system/src/services/member/car/car_services.js
@@ -0,0 +1,98 @@
+import axios from "axios";
+import authHeader from "../auth_header";
+
+const API_URL = "http://localhost:8010/admin/car-func/";
+
+class CarService {
+  getAllBrands() {
+    return axios
+      .get(API_URL + "findAllBrands")
+      .then((response) => {
+        //console.log(response.data);
+        return response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getCarsByBrand(brand) {
+    console.log("Get Brand: " + brand);
+    return axios
+      .post(API_URL + "findByBrand", { brand })
+      .then((response) => {
+        //console.log(response.data.cars[0]);
+        return response.data.cars;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getAllCars() {
+    return axios
+      .get(API_URL + "findAll")
+      .then((response) => {
+        //console.log(response.data);
+        return response.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  addCar(name, brand) {
+    return axios
+      .post(
+        API_URL + "addCar",
+        {
+          name,
+          brand,
+        },
+        {
+          headers: authHeader(),
+        }
+      )
+      .then((response) => {
+        return response.data.message;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteCar(carId) {
+    return axios
+      .delete(API_URL + `deleteCar/${carId}`, {
+        headers: authHeader(),
+      })
+      .then((res) => {
+        console.log(res);
+        return res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  updateCar(carId, brand) {
+    return axios
+      .patch(
+        API_URL + `updateCar/${carId}`,
+        {
+          brand,
+        },
+        {
+          headers: authHeader(),
+        }
+      )
+      .then((response) => {
+        return response.data.message;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
+
+export default new CarService();
